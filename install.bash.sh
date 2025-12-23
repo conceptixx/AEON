be5291766ffdc3625c0e174c6a4dc8de3e6484ca
Nicolas Hoeller <conceptixx@gmail.com>
2025-12-23 22:37:55 +0100

    testing and bugfixing process

--- install.bash.sh
+++ install.bash.sh
@@ -515,7 +515,7 @@ setup_directories() {
 }
 
 source_install_env_post_clone() {
-    local env_file="${AEON_ROOT}/${AEON_INSTALL_ENV_REL_PATH}"
+    local env_file="${AEON_ROOT}/${AEON_ORCH_REPO}/${AEON_INSTALL_ENV_REL_PATH}"
 
     if [ ! -f "$env_file" ]; then
         log "No install.env found to source (expected at $env_file)"
